3965c0116281386f631213cee1d58f8130443bb0
sami <s.a.sami359359@gmail.com>
2023-10-17 08:56:21 +0530

    product not found fixes 17

--- app/api/getUserFromSession/route.js
+++ app/api/getUserFromSession/route.js
@@ -2,19 +2,19 @@ import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
 export async function GET(req, res) {
     try {
-        
+
         const session = await getServerSession();
-        if(session){
+        if (session) {
             if (Object.keys(session.user).includes("image")) {
-                return new NextResponse(JSON.stringify({ email: session.user.email, name: session.user.name, imgUrl: session.user.image }))
+                return new NextResponse(JSON.stringify({ email: session?.user?.email, name: session?.user?.name, imgUrl: session?.user?.image }))
             }
-            return new NextResponse(JSON.stringify({ email: session.user.email, name: session.user.name }))
+            return new NextResponse(JSON.stringify({ email: session?.user?.email, name: session?.user?.name }))
         }
-        else{
-            return new NextResponse(JSON.stringify({ msg: "error" }))   
+        else {
+            return new NextResponse(JSON.stringify({ msg: "error" }))
         }
-        }
-       
+    }
+
     catch (err) {
         return new NextResponse(JSON.stringify({ msg: "error" }))
     }
