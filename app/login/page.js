d160aa3e0ddab9b4ac26029a4c21c6353a05719c
sami <s.a.sami359359@gmail.com>
2023-10-17 15:46:10 +0530

    minor fixes 2

--- app/login/page.js
+++ app/login/page.js
@@ -100,8 +100,8 @@ const router = useRouter();
           <div className='flex justify-center items-center flex-col lg:flex-row gap-5  w-full '>
 
 
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 h-96 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div  data-aos="fade-up"  className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div data-aos="fade-up" className="w-full bg-white rounded-lg shadow dark:border md:mt-0 h-96 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div    className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
                 </h1>
--- app/signup/page.js
+++ app/signup/page.js
@@ -114,8 +114,8 @@ export default function Signup() {
             <img   data-aos="fade-up" className="w-16 h-16 mr-3 bg-black" src="/images/transparent X logo.png" alt="logo" />
             Store
           </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div  data-aos="fade-up" className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <div data-aos="fade-up" className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div   className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1  data-aos="fade-up" className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create an Account
               </h1>
