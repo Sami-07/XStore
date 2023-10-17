6289804f986c5282a88b2ba6dfaf4a3369c1b604
sami <s.a.sami359359@gmail.com>
2023-10-17 15:05:39 +0530

    use effect fixes

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -59,7 +59,7 @@ export default function Slug({ params }) {
 
     }
     fetching();
-  }, [])
+  }, [selectedColor, selectedSize])
   // selectedColor, selectedSize, params.slug
   function redirectUrl(selectedSize, selectedColor) {
     let newUrl = `${process.env.NEXT_PUBLIC_HOST}/product/${colorSizeSlug[selectedColor][selectedSize]["slug"]}`
