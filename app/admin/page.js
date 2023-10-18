c735a1d0555eac97e01c066b19b4639f78e80c8b
sami <s.a.sami359359@gmail.com>
2023-10-18 22:56:06 +0530

    admin UI fixes

--- app/admin/page.js
+++ app/admin/page.js
@@ -31,12 +31,12 @@ export default function Admin() {
     <div className='flex flex-col justify-center items-center my-5 gap-3 '>
       {isLoading && <CustomLoader />}
       {isAdmin && <div className='mt-32 md:mt-20 flex flex-col justify-center items-center gap-4'>
-      <h1>Admin Panel</h1>
-        <Link className='bg-purple-500 text-white p-2 ' href="/admin/addproducts">Add Product</Link>
-        <Link className='bg-purple-500 text-white p-2 ' href="/admin/allorders">View All Orders</Link>
-        <Link className='bg-purple-500 text-white p-2 ' href="/admin/allproducts">View Inventory/Stock</Link>
-        <Link className='bg-purple-500 text-white p-2 ' href="/admin/allusers">View Inventory/Stock</Link>
-       
+        <h1 className='text-2xl font-semibold'>Admin Panel</h1>
+        <Link className='my-bg-color rounded-md text-white p-2 ' href="/admin/addproducts">Add Product</Link>
+        <Link className='my-bg-color rounded-md text-white p-2 ' href="/admin/allorders">View All Orders</Link>
+        <Link className='my-bg-color rounded-md text-white p-2 ' href="/admin/allproducts">View Inventory/Stock</Link>
+        <Link className='my-bg-color rounded-md text-white p-2 ' href="/admin/allusers">View All Usersk</Link>
+
       </div>}
     </div>
 
